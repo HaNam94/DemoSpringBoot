fceeec2c07759ea639b0a8b8a759c6105c4fdd0f
HaNam94 <haduynam.94@gmail.com>
2024-06-30 19:26:10 +0700

    demo springboot

---
+++ src/main/java/com/example/customermanagement/model/Customer.java
@@ -0,0 +1,18 @@
+package com.example.customermanagement.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+
+@Entity
+@Table(name = "customer")
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
